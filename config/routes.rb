197ab31af921030a1c6dbc4c03953152571b1080
Joshua Daniel <jrdaniel@ncsu.edu>
2016-07-12 16:47:54 -0400

    Added Artist New Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "application#about"
+
 get "about", to: "application#about"
+
 get "artists", to: "artists#index"
+get "artists/new", to: "artists#new"
+get "artists/:id", to: "artists#show", as: :artist
 end
